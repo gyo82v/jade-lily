a244cb07abd3749bafbb83698474c2f78e1b1dff
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-11-14 20:39:35 +0100

    feat: add header structure

--- app/layout/Header.tsx
+++ app/layout/Header.tsx
@@ -1,7 +1,17 @@
 export default function Header(){
+    const header = `flex items-center justify-between px-4 py-6`
+    const h1 = `text-3xl font-bold 
+                text-transparent bg-clip-text 
+                bg-gradient-to-br from-orange-950 via-orange-600 to-orange-950`
     return(
-        <header>
-            <h1>Jade lily</h1>
+        <header className={header}>
+            <h1 className={h1}>Jade lily</h1>
+            <section className="flex">
+                <p>About</p>
+                <p>menu</p>
+                <p>order</p>
+                <p>log in</p>
+            </section>
         </header>
     )
 }
