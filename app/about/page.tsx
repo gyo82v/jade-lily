f43fb4f39a93826c2847338dd80d2fd4dc43a7ea
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-11-20 23:37:16 +0100

    style: add style for larger screen in the about page component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,61 +3,47 @@ import Navlink from "@/components/Navlink"
 import Review from "@/components/Review"
 
 export default function AboutPage(){
-    const imageStyle = ``
-    const container = ``
+    const bgStyle = `bg-gradient-to-br from-orange-100 to-orange-50 shadow-lg rounded-lg`
+    const flex = `flex flex-col`
 
-    const section1 = ``
-
-    const section2 = `p-4`
-    const div1Sec2 = `my-6`
-    const div2Sec2 = `bg-gradient-to-br from-orange-100 to-orange-50 px-4 py-8 shadow-lg rounded-lg 
-                      my-4 flex flex-col gap-6 text-orange-800`
-    const pSec2 = ``
-
-    const section3 = `p-4 `
-    const divSec3 = `bg-gradient-to-br from-orange-100 to-orange-50 shadow-lg rounded-lg px-4 py-6 
-                     flex flex-col gap-6`
-    const div1Sec3 = ``
-    const psec3 = `font-bold text-lg`
-    const li = `text-center border-2 border-orange-800 rounded-lg shadow-lg p-2 text-lg font-semibold 
-                flex-1 hover:scale-105 active:scale-95 hover:shadow-xl hover:bg-orange-100
-                transition-transform transition-shadow transition-colors duration-300 ease-in-out`
-
-
+    const liWrapLink = `text-center border-2 border-orange-800 rounded-lg shadow-lg p-2 text-lg font-semibold 
+                        flex-1 hover:scale-105 active:scale-95 hover:shadow-xl hover:bg-orange-100
+                        transition-transform transition-shadow transition-colors duration-300 ease-in-out`
 
     return(
-        <div className={container}>
-            <section className={section1}>
+        <div className="md:w-10/12 md:mx-auto">
+            <section className="">
                 <Image 
                   src="/about.jpg"
                   width={1920} 
                   height={1272} 
                   alt={`A small glass of water, lots of colored hearts on the background,
                         all surrounded by a dark warm light`} 
-                  className={imageStyle} 
+                  className="md:h-120" 
                 />
             </section>
-            <section className={section2}>
-                <div className={div1Sec2}>
-                    <h1 className="font-bold text-2xl text-center">Why settle for ordinary when you can dine at Jade Lily?</h1>
-                    <div className={div2Sec2}>
-                        <p className={pSec2}>
+            <section className={`${flex} p-4 md:flex-row md:items-stretch md:gap-2`}>
+                <div className="my-6 md:flex-1 md:flex md:flex-col md:my-0">
+                    <h1 className="font-bold text-2xl text-center md:flex-1 md:flex md:justify-center md:items-center">Why settle for ordinary when you can dine at Jade Lily?</h1>
+                    <div className={`${bgStyle} ${flex} px-4 py-8 my-4 gap-6 text-orange-800 md:flex-5
+                                     md:justify-evenly md:text-lg`}>
+                        <p>
                             Jade Lily began with a passion for creating exceptional dining experiences.
                             Our journey started in a small kitchen and has grown into a destination for
                             food lovers.
                         </p>
-                        <p className={pSec2}>
+                        <p >
                             Our mission is to enhance your dining experience with the finest dishes.
                             Our meals are crafted with care to ensure every bite is memorable.
                         </p>
-                        <p className={pSec2}>
+                        <p>
                             Our chefs are passionate about food and understand the art of creating exceptional
                             dining experiences.
                         </p>
                     </div>
                 </div>
-                <div>
-                    <ul className="flex flex-col gap-3">
+                <div className="md:flex-1">
+                    <ul className={`${flex} gap-3`}>
                         <li>
                             <Review rating={5} author="Sarah Thompson" date="2025-03-15">
                                 Jade Lily is a hidden gem! The avocado salad was fresh and flavorful,
@@ -94,21 +80,21 @@ export default function AboutPage(){
                             </Review>
                         </li>
                     </ul>
-
                 </div>
             </section>
-            <section className={section3}>
-                <div className={divSec3}>
-                    <div className={div1Sec3}>
-                        <p className={psec3}>Your table is waiting,</p>
-                        <p className={psec3}>Your meal is ready.</p>
+            <section className="p-4">
+                <div className={`${bgStyle} ${flex} md:gap-10 md:py-10 px-4 py-6 gap-6`}>
+                    <div className={`${flex} md:flex-row md:items-center md:justify-center font-bold text-lg
+                                             md:text-2xl md:gap-1 `}>
+                        <p>Your table is waiting,</p>
+                        <p>Your meal is ready.</p>
                     </div>
                     <div className="w-full">
                         <ul className="flex gap-6 w-full">
-                            <li className={li}>
+                            <li className={liWrapLink}>
                                 <Navlink href="/" className="block">Join us</Navlink>
                             </li>
-                            <li className={li}>
+                            <li className={liWrapLink}>
                                 <Navlink href="/" className="block">Contact us</Navlink>
                             </li>
                         </ul>
