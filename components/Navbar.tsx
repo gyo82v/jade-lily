76430897ef46687931c6c456198347c828d8d39a
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-11-24 21:41:39 +0100

    style: add atyle to the navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import type { NavBarProps } from "@/types"
 
 export function Navbar({children}:NavBarProps){
     const nav = ``
-    const ul = `flex gap-2`
+    const ul = `flex gap-2 overflow-x-auto  whitespace-nowrap scrollbar-hide`
     return(
         <nav className={nav}>
             <ul className={ul}>
