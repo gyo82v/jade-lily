cd82dd7331d585bc86b005909ea94d6b197a739b
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-11-28 16:58:12 +0100

    feat: add types to the database object

--- app/menu/desserts/page.tsx
+++ app/menu/desserts/page.tsx
@@ -1,10 +1,20 @@
 import { getItems } from "@/firebase/dishCollectionAdmin"
-
+import { DishList } from "@/components/dishes"
 
 export default async function DessertsPage(){
-    const data = await getItems("category", "desserts")
-    console.log(data)
+    let data = []
+    try {
+        const data = await getItems("category", "desserts")
+    }catch(err){
+        console.error("there wae an error in getItems: ", err)
+    }
+
     return(
-        <p>desserts here</p>
+        <div>
+            <section>
+                <p>Filters here</p>
+            </section>
+            <DishList data={data} />
+        </div>
     )
 }
---
+++ components/dishes/DishCard.tsx
@@ -0,0 +1,7 @@
+export function DishCard({data}){
+    return(
+        <div>
+            <h1>dishcard here</h1>
+        </div>
+    )
+}
