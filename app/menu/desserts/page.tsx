ea59c4eb63299c7c4d1bf58b06fecbd13d510caf
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-11-28 14:51:13 +0100

    feat: add server function to get data in the dessert page

--- app/menu/desserts/page.tsx
+++ app/menu/desserts/page.tsx
@@ -1,4 +1,9 @@
-export default function DessertsPage(){
+import { getItems } from "@/firebase/dishCollectionAdmin"
+
+
+export default async function DessertsPage(){
+    const data = await getItems("category", "desserts")
+    console.log(data)
     return(
         <p>desserts here</p>
     )
