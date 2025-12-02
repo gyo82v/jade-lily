dbd85d4b2b09196fe4ed0f09e992464f88e30ddb
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-12-02 19:00:58 +0100

    feat: add lib folder for all utilities functions

---
+++ lib/utilsAdmin.ts
@@ -0,0 +1,14 @@
+import type { DishProps } from "@/types"
+import { getItems } from "@/firebase/dishCollectionAdmin"
+
+export async function getFilteredItems(value:string, key:string) {
+   let data:DishProps[] = []
+
+   try{
+    data = await getItems(value, key) as DishProps[]
+    return data
+   }catch(err){
+    console.error(`there was an error in the getItems function: `, err)
+    return []
+   }
+}
