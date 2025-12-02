2eb81d7accfd9c9ada823378edce91bf9881f219
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-12-02 18:20:15 +0100

    feat: add the icon type element in the dish card component

--- components/dishes/DishCard.tsx
+++ components/dishes/DishCard.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image"
 import Link from "next/link"
+import { FaPepperHot, FaLeaf, FaUtensils, FaRegStar } from 'react-icons/fa';
+import { 
+         GiGrapes, GiFrenchFries, GiCarrot,
+         GiWineGlass, GiGlassShot, GiChocolateBar,
+} from 'react-icons/gi';
 
 import type { DishProps } from "@/types"
 
@@ -8,6 +13,7 @@ type Props = {
 }
 
 export function DishCard({data}:Props){
+    const {slug, imageUrlThumb, description, name, type, price} = data
     console.log("card data: ", data)
 
     //tailwind
@@ -18,32 +24,53 @@ export function DishCard({data}:Props){
      const imgStyle = `rounded-lg w-full shadow-lg`
 
      const section = ``
-     const div = `flex justify-between font-bold text-xl`
+     const div = `flex justify-between font-bold text-lg mb-2`
      const h1 = `font-dancing`
      const p = ``
-
-
+     const iconTypeContainer = `bg-gradient-to-br flex items-center justify-center rounded-lg py-1 w-1/2
+                                ${type === "Vegetarian" ? "from-green-200 via-green-300 to-green-500 text-green-800":
+                                  type === "Spicy" ? "from-red-200 via-red-300 to-red-500 text-red-900":
+                                  type === "Special" ? "from-amber-200 via-amber-300 to-amber-500 text-amber-900":
+                                  type === "Salad" ? "from-lime-200 via-lime-300 to-lime-500 text-lime-900":
+                                  type === "Starter" ? "from-orange-200 via-orange-300 to-orange-500 text-orange-900":
+                                  type === "Fruits" ? "from-purple-200 via-purple-300 to-purple-500 text-purple-900":
+                                  type === "Choco" ? "from-yellow-800 via-yellow-900 to-yellow-950 text-yellow-100":
+                                  type === "Alchool" ? "from-fuchsia-200 via-fuchsia-300 to-fuchsia-500 text-fuchsia-900":
+                                  type === "No-alchool" ? "from-teal-200 via-teal-300 to-teal-500 text-teal-900":
+                                                          "from-indigo-200 via-indigo-300 to-indigo-500 text-indigo-900"
+                                }`
     //
+
+    const iconType = type === "Vegetarian" ? <FaLeaf /> :
+                     type === "Spicy" ? <FaPepperHot /> :
+                     type === "Special" ? <FaRegStar /> :
+                     type === "Salad" ? <GiCarrot /> :
+                     type === "Starter" ? <GiFrenchFries /> :
+                     type === "Fruits" ? <GiGrapes /> :
+                     type === "Choco" ? <GiChocolateBar /> :
+                     type === "Alchool" ? <GiWineGlass /> :
+                     type === "No-alchool" ? <GiGlassShot /> : <FaUtensils />
+                     
 
     return(
         <article className={article}>
-            <Link href={`/menu/desserts/${data.slug}`} className={link}>
+            <Link href={`/menu/desserts/${slug}`} className={link}>
                 <figure className={figure}>
                     <Image 
-                      src={data.imageUrlThumb} 
+                      src={imageUrlThumb} 
                       width={300} 
                       height={200} 
-                      alt={data.description} 
+                      alt={description} 
                       className={imgStyle} 
                     />
                 </figure>
                 <section className={section}>
                     <div className={div}>
-                        <h1 className={h1}>{data.name}</h1>
-                        <p className={p}>£{data.price}</p>
+                        <h1 className={h1}>{name}</h1>
+                        <p className={p}>£{price}</p>
                     </div>
-                    <div>
-                        <p>{data.type}</p>
+                    <div className={iconTypeContainer}>
+                        {iconType}
                     </div>
                 </section>               
             </Link>
