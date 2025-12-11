872b71d04f92a2e202368843675d0eb4fa577ad0
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-12-11 19:53:52 +0100

    refactor: deleted unused functions

--- lib/utilsAdmin.tsx
+++ lib/utilsAdmin.tsx
@@ -1,8 +1,5 @@
 import type { DishProps } from "@/types"
 import { getItems } from "@/firebase/dishCollectionAdmin"
-import {FaPepperHot, FaLeaf, FaUtensils, FaRegStar} from 'react-icons/fa';
-import {GiGrapes, GiFrenchFries, GiCarrot, GiWineGlass, GiGlassShot, GiChocolateBar} from 'react-icons/gi';
-import { FiRefreshCw } from 'react-icons/fi';
 
 export async function getFilteredItems(value:string, key:string) {
    let data:DishProps[] = []
@@ -19,15 +16,3 @@ export function getFilterArray(dishesArr:DishProps[]){
    return [...new Set(dishesArr.map(dish => dish.type).filter(Boolean))]
 }
 
-export function renderIcon(filter?:string){
-   return filter === "Spicy" ? <FaPepperHot /> :
-          filter === "Fruits" ? <GiGrapes /> :
-          filter === "Choco" ? <GiChocolateBar /> :
-          filter === "Alchool" ? <GiWineGlass /> :
-          filter === "No-alchool" ? <GiGlassShot /> :
-          filter === "Special" ? <FaRegStar /> :
-          filter === "Regular" ? <FaUtensils /> :
-          filter === "Vegetarian" ? <FaLeaf /> :
-          filter === "Salad" ? <GiCarrot /> :
-          filter === "Starter" ? <GiFrenchFries /> : <FiRefreshCw />
-}
