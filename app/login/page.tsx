e446544fc86dc521487e08feb9955bd851d8b062
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-12-19 19:43:07 +0100

    feat: add login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+
+import { LoginForm } from "@/components/forms";
+import ClientProviders from "@/firebase/ClientProviders";
+
 export default function LoginPage(){
+ 
     return(
-        <p>log in here</p>
+        <ClientProviders>
+            <section>
+                <h1>log in to your account</h1>
+                <LoginForm />
+            </section>
+        </ClientProviders>
     )
 }
