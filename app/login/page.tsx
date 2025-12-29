e07f30eeeabbbfd6d54d90c585f17825eb0a7570
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-12-29 21:10:25 +0100

    feat: update the log in page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,13 +5,14 @@ import ClientProviders from "@/firebase/ClientProviders";
 import Link from "next/link";
 
 export default function LoginPage(){
+    const link = `text-orange-700 font-bold`
  
     return(
         <ClientProviders>
             <section>
                 <h1>log in to your account</h1>
                 <LoginForm />
-                <p>Dont have an account?<Link href="/signup">Create one now</Link></p>
+                <p>Dont have an account?<Link href="/signup" className={link}>Create one now</Link></p>
             </section>
         </ClientProviders>
     )
