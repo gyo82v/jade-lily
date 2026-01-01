eb7c62c4a679912cd94d2c33caf53302e5d415fc
gyo82v <gyo82v@gmail.com>
2026-01-01 13:52:58 +0100

    feat add hamburger icon to the header

--- app/layout/Navbar.tsx
+++ app/layout/Navbar.tsx
@@ -3,6 +3,8 @@
 import { usePathname } from "next/navigation";
 import { Navlink } from "@/components";
 import { useAuth } from "@/firebase/authProvider";
+import { HiMenu } from "react-icons/hi";
+
 
 export default function Navbar(){
     const {user, profile} = useAuth()
@@ -13,35 +15,39 @@ export default function Navbar(){
     const p = `px-4`
     return(
         <nav className={nav}>
-            <ul className={ul}>
-                <li>
-                    <Navlink href="/" isActive={pathname === "/"}>Home</Navlink>
-                </li>
-                <li>
-                    <Navlink href="/about" isActive={pathname.startsWith("/about")}>About</Navlink>
-                </li>
-                <li>
-                    <Navlink href="/menu" isActive={pathname.startsWith("/menu")}>Menu</Navlink>
-                </li>
-            </ul>
-            {
-                user ? 
+            <div className="hidden md:block">
+                <ul className={ul}>
+                   <li>
+                       <Navlink href="/" isActive={pathname === "/"}>Home</Navlink>
+                   </li>
+                   <li>
+                       <Navlink href="/about" isActive={pathname.startsWith("/about")}>About</Navlink>
+                   </li>
+                   <li>
+                       <Navlink href="/menu" isActive={pathname.startsWith("/menu")}>Menu</Navlink>
+                   </li>
+               </ul>
+               {
+                  user ? 
 
-                <ul className={div}>
-                    <Navlink href="/account">{profile?.displayName}</Navlink>
-                    <p className={p}>log out here</p>
-                </ul>
-                :
-
-                 <ul className={div}>
-                <li>
-                    <Navlink href="/account" isActive={pathname.startsWith("/account")}>Account</Navlink>
-                </li>
-                <li>
-                    <Navlink href="/login" isActive={pathname.startsWith("/login")}>log in</Navlink>
-                </li>
-            </ul>
-            }
+                   <ul className={div}>
+                       <Navlink href="/account">{profile?.displayName}</Navlink>
+                       <p className={p}>log out here</p>
+                   </ul>
+                   :
+                   <ul className={div}>
+                      <li>
+                        <Navlink href="/account" isActive={pathname.startsWith("/account")}>Account</Navlink>
+                     </li>
+                     <li>
+                        <Navlink href="/login" isActive={pathname.startsWith("/login")}>log in</Navlink>
+                     </li>
+                   </ul>
+                }
+            </div>
+            <div className="md:hidden">
+                <HiMenu />
+            </div>
         </nav>
     )
 }
