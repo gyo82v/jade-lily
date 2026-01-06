eb1769f37f30b518fa86dd09145657151c28fbf3
gyo82v <gyo82v@gmail.com>
2026-01-06 17:43:26 +0100

    feat: update the sign up page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,10 +2,11 @@ import {SignupForm} from "@/components/forms"
 import ClientProviders from "@/firebase/ClientProviders";
 
 export default function SignupPage() {
+  const sectionStl = "p-4";
   return (
     <ClientProviders>
-        <section>
-            <h1>Sign up here</h1>
+        <section className={sectionStl}>
+            <h1 className="text-xl font-bold">Create new account</h1>
             <SignupForm />
         </section>
     </ClientProviders>
