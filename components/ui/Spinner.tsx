2ecb15d9561e6c69cd25228caa58eaf317cad3a7
gyo82v <gyo82v@gmail.com>
2026-01-07 19:58:19 +0100

    feat: add a ui folder and a spinner custom component

---
+++ components/ui/Spinner.tsx
@@ -0,0 +1,28 @@
+type Props = {
+    className?: string;
+}   
+export function Spinner({className=""}: Props){
+    return(
+    <svg
+      className={`animate-spin ${className}`}
+      viewBox="0 0 24 24"
+      fill="none"
+      aria-hidden="true"
+    >
+      <circle
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        strokeWidth="4"
+        opacity="0.25"
+      />
+      <path
+        d="M22 12a10 10 0 0 1-10 10"
+        stroke="currentColor"
+        strokeWidth="4"
+        strokeLinecap="round"
+      />
+    </svg>
+    )
+}
