075d55a173010ca1f259267edbd6731d319f100b
gyo82v <gyo82v@gmail.com>
2026-01-07 19:56:26 +0100

    feat: update the sign up, sign in and sign out components

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,19 +1,19 @@
-"use client";
-
 import { LoginForm } from "@/components/forms";
 import ClientProviders from "@/firebase/ClientProviders";
 import Link from "next/link";
 
 export default function LoginPage(){
     const link = `text-orange-700 font-bold ml-1`
-    const section = `p-4`
- 
+
     return(
         <ClientProviders>
-            <section className={section}>
-                <h1  className="text-xl font-bold">Log in to your account</h1>
+            <section className="p-4 mx-auto max-w-lg md:mt-14">
+                <h1  className="text-xl font-bold">Sign in to your account</h1>
                 <LoginForm />
-                <p>Dont have an account?<Link href="/signup" className={link}>Create one now</Link></p>
+                <p className="text-center">
+                    Dont have an account?
+                    <Link href="/signup" className={link}>Create one now</Link>
+                </p>
             </section>
         </ClientProviders>
     )
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,13 +1,19 @@
 import {SignupForm} from "@/components/forms"
 import ClientProviders from "@/firebase/ClientProviders";
+import Link from "next/link";
 
 export default function SignupPage() {
-  const sectionStl = "p-4";
+  const link = `text-orange-700 font-bold ml-1`
+
   return (
     <ClientProviders>
-        <section className={sectionStl}>
+        <section className="p-4 mx-auto max-w-lg md:mt-14">
             <h1 className="text-xl font-bold">Create new account</h1>
             <SignupForm />
+            <p className="text-center">
+              Already have an account?
+              <Link href="/sign-in" className={link}>Sign in now</Link>
+            </p>
         </section>
     </ClientProviders>
   )
