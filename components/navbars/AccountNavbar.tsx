07cd1ca3eca66104a8feb31e36be2efbb8758cfe
gyo82v <gyo82v@gmail.com>
2026-01-12 18:57:02 +0100

    feat: update the navbar and account navbar components

--- components/navbars/AccountNavbar.tsx
+++ components/navbars/AccountNavbar.tsx
@@ -3,21 +3,25 @@ import { Navlink } from "./Navlink";
 
 export function AccountNavbar() {
     return (
-        <Navbar>
+        <Navbar 
+          className="flex items-center justify-center w-full mx-2 " 
+          classNameUl={`w-full justify-evenly px-2
+                      text-lg font-semibold`}
+        >
             <li>
-                <Navlink href="/account">Dashboard</Navlink>
+                <Navlink href="/account" >Dashboard</Navlink>
             </li>
             <li>
-                <Navlink href="/account/menu">Menu</Navlink>
+                <Navlink href="/account/menu" >Menu</Navlink>
             </li>
             <li>
-                <Navlink href="/account/cart">Cart</Navlink>
+                <Navlink href="/account/cart" >Cart</Navlink>
             </li>
             <li>
-                <Navlink href="/account/orders">Orders</Navlink>
+                <Navlink href="/account/orders" >Orders</Navlink>
             </li>
             <li>
-                <Navlink href="/account/settings">Settings</Navlink>
+                <Navlink href="/account/settings" >Settings</Navlink>
             </li>
         </Navbar>
     )
--- components/navbars/Navbar.tsx
+++ components/navbars/Navbar.tsx
@@ -1,11 +1,16 @@
-import type { NavBarProps } from "@/types"
 
-export function Navbar({children}:NavBarProps){
-    const nav = ` w-full max-w-full overflow-x-auto overflow-y-hidden scrollbar-hide`
-    const ul = `inline-flex gap-2 whitespace-nowrap min-w-max `
+type Props = {
+    className? : string
+    children : React.ReactNode
+    classNameUl? : string
+}
+
+export function Navbar({children, className, classNameUl}: Props){
+    const nav = `max-w-full overflow-x-auto overflow-y-hidden scrollbar-hide`
+    const ul = `inline-flex whitespace-nowrap min-w-max `
     return(
-        <nav className={nav}>
-            <ul className={ul}>
+        <nav className={`${nav} ${className || "w-full"}`}>
+            <ul className={`${ul} ${classNameUl || "gap-2"}`}>
                 {children}
             </ul>
         </nav>
