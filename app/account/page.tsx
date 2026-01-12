1c5f7e2aac5563719a2845f2727d9a1700feb858
gyo82v <gyo82v@gmail.com>
2026-01-12 18:58:32 +0100

    feat: update the dashboard page in the account path

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,14 +1,25 @@
+"use client"
+
 import Link from "next/link"
 import { FaArrowRight } from "react-icons/fa";
+import { useAuth } from "@/firebase/authProvider";
 
 export default function AccountPage(){
-    const sectionStyle = `flex justify-between p-4 bg-gradient-to-br 
+    const {user, profile, loading} = useAuth();
+    console.log("user profile:", profile);
+    console.log("user auth:", user);
+    console.log("loading auth state:", loading);
+
+    const sectionStyle = `flex justify-between py-14 px-2 bg-gradient-to-br font-semibold text-lg
                           from-orange-100 to-orange-50 rounded-lg shadow-md`
     const linkStyle = `flex items-center gap-2 `
     return(
-        <section className="flex flex-col gap-4 p-4">
+        <section className="flex flex-col h-full justify-evenly gap-4 p-4 w-full mt-6">
             <div className={sectionStyle}>
-                <p>manage your account</p>
+                <div>
+                    <p className="font-bold text-xl">Welcome back <span className="font-dancing text-2xl ml-1">{profile?.displayName || "User"}</span>!</p>
+                    <p className="font-light">Manage your account</p>
+                </div>
                 <Link href="/account/settings" className={linkStyle}>Details <FaArrowRight /></Link>
             </div>
             <div className={sectionStyle}>
@@ -16,11 +27,11 @@ export default function AccountPage(){
                 <Link href="/account/menu" className={linkStyle}>Details <FaArrowRight /></Link>
             </div>
             <div className={sectionStyle}>
-                <p>your cart : 0 items</p>
+                <p>Your cart : {profile?.jadeLilyCart?.length} items</p>
                 <Link href="/account/cart" className={linkStyle}>Details <FaArrowRight /></Link>
             </div>
             <div className={sectionStyle}>
-                <p>book a table</p>
+                <p>Book a table</p>
                 <Link href="/account/reservations" className={linkStyle}>details <FaArrowRight /></Link>
             </div>
         </section>
