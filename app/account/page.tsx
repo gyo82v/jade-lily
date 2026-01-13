197e0bb8ec0ee4ce78574ea5e63d75be4278b09f
gyo82v <gyo82v@gmail.com>
2026-01-13 18:31:16 +0100

    feat: update the account dashboard page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Link from "next/link"
-import { FaArrowRight } from "react-icons/fa";
+import { FaArrowRight, FaArrowDown } from "react-icons/fa";
 import { useAuth } from "@/firebase/authProvider";
 
 export default function AccountPage(){
@@ -10,30 +10,61 @@ export default function AccountPage(){
     console.log("user auth:", user);
     console.log("loading auth state:", loading);
 
-    const sectionStyle = `flex justify-between py-14 px-2 bg-gradient-to-br font-semibold text-lg
-                          from-orange-100 to-orange-50 rounded-lg shadow-md`
-    const linkStyle = `flex items-center gap-2 `
+    const containerStyle = `flex flex-col h-full justify-evenly gap-4 p-4 w-full mt-6 
+                            md:grid md:grid-cols-2 md:gap-10 md:mt-12 md:px-18`;
+
+    const sectionStyle = `flex md:flex-col justify-between items-center py-14 px-2 bg-gradient-to-br font-semibold text-lg
+                          from-orange-100 to-orange-50 rounded-lg shadow-md 
+                          hover:scale-105 transition-transform duration-300 ease-in-out
+                          active:scale-95 
+                          focus-visible:scale-105 focus-visible:outline-none focus-visible:ring-2
+                          focus-visible:ring-offset-2 focus-visible:ring-orange-300`;
+    const linkStyle = `flex items-center gap-2 md:flex md:flex-col `
     return(
-        <section className="flex flex-col h-full justify-evenly gap-4 p-4 w-full mt-6">
-            <div className={sectionStyle}>
+        <section className={containerStyle}>
+            <Link href="/account/settings" className={sectionStyle}>
                 <div>
                     <p className="font-bold text-xl">Welcome back <span className="font-dancing text-2xl ml-1">{profile?.displayName || "User"}</span>!</p>
                     <p className="font-light">Manage your account</p>
                 </div>
-                <Link href="/account/settings" className={linkStyle}>Details <FaArrowRight /></Link>
-            </div>
-            <div className={sectionStyle}>
+                <p className={linkStyle}>
+                    Details 
+                    <span>
+                        <FaArrowRight className="md:hidden" /> 
+                        <FaArrowDown className="hidden md:block" />
+                    </span>
+                </p>
+            </Link>
+            <Link href="/account/menu" className={sectionStyle}>
                 <p>Order online</p>
-                <Link href="/account/menu" className={linkStyle}>Details <FaArrowRight /></Link>
-            </div>
-            <div className={sectionStyle}>
+                <p className={linkStyle}>
+                    Details 
+                    <span>
+                        <FaArrowRight className="md:hidden" /> 
+                        <FaArrowDown className="hidden md:block" />
+                    </span>
+                </p>
+            </Link>
+            <Link href="/account/cart" className={sectionStyle}>
                 <p>Your cart : {profile?.jadeLilyCart?.length} items</p>
-                <Link href="/account/cart" className={linkStyle}>Details <FaArrowRight /></Link>
-            </div>
-            <div className={sectionStyle}>
+                <p className={linkStyle}>
+                    Details 
+                    <span>
+                        <FaArrowRight className="md:hidden" /> 
+                        <FaArrowDown className="hidden md:block" />
+                    </span>
+                </p>
+            </Link>
+            <Link href="/account/reservations" className={sectionStyle}>
                 <p>Book a table</p>
-                <Link href="/account/reservations" className={linkStyle}>details <FaArrowRight /></Link>
-            </div>
+                <p className={linkStyle}>
+                    Details 
+                    <span>
+                        <FaArrowRight className="md:hidden" /> 
+                        <FaArrowDown className="hidden md:block" />
+                    </span>
+                </p>
+            </Link>
         </section>
     )
 }
