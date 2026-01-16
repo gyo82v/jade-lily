52b62c08f8885a40d3cd4e40ca6b6341179ff01c
gyo82v <gyo82v@gmail.com>
2026-01-16 19:51:26 +0100

    feat: update the add-credit page

--- app/account/add-credit/page.tsx
+++ app/account/add-credit/page.tsx
@@ -1,3 +1,10 @@
+import { AddCreditForm } from "@/components/forms";
+
 export default function AddCreditPage() {
-    return <div>Add Credit Page</div>;
+    return (
+    <section className="flex flex-col mt-10">
+        <h1 className="text-xl font-bold text-center">Add Credit to your account</h1>
+        <AddCreditForm />
+    </section>
+    );
 }
--- app/sign-out/page.tsx
+++ app/sign-out/page.tsx
@@ -1,34 +1,27 @@
 "use client"
 
 import { signOut } from "@/firebase/authClient"
-import { useState } from "react"
 import { Button } from "@/components"
 import { primaryButtonStyles } from "@/components/styles"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 
 export default function LogoutPage(){
-    const [isLoggedOut, setIsLoggedOut] = useState(false)
-    const handleLogout = async () => {
+    const router = useRouter()
+    const handleSignOut = async () => {
         await signOut()
-        setIsLoggedOut(true)
+        router.replace("/sign-in?signed-out=1")
     }
 
     return(
         <section className="p-4 flex flex-col items-center w-full">
-            {isLoggedOut ?
-             (<div>
-                <p className="text-2xl font-bold mt-20">You have signed out.</p>
-             </div>)  :
-            (
             <div className="flex flex-col items-center gap-8 mx-auto mt-20 w-10/12 ">
               <p className="text-2xl font-bold text-center">Are you sure you want to sign out?</p>
               <div className="flex gap-4 w-full ">
-                   <Button onClick={handleLogout} className="flex-1">Yes</Button>
+                   <Button onClick={handleSignOut} className="flex-1">Yes</Button>
                    <Link href="/" className={`${primaryButtonStyles} flex-1`}>No</Link>
               </div>
-            </div>
-        ) 
-          }  
+            </div>    
         </section>
     )
 }
