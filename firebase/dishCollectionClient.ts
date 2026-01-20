16b44b4b79b105160af7623829893af75ecbaf37
gyo82v <gyo82v@gmail.com>
2026-01-20 20:02:07 +0100

    feat: update the firebase client file for the jade-lily collection

--- firebase/dishCollectionClient.ts
+++ firebase/dishCollectionClient.ts
@@ -1,20 +1,47 @@
 import { db } from "./firebase";
-import {collection, doc, getDoc, getDocs, query, where} from "firebase/firestore"
+import {collection, doc, getDoc, getDocs, query, where, QueryConstraint} from "firebase/firestore"
+import type { DishProps } from "@/types";
 
 const collectionRef = collection(db, "jade-lily")
 
 //fetch data that matches the parameter if provided. Otherwise fecth everything;client side
 
-export async function getItems(key?:string, value?:string){
-    let q
-    if(key && value){
-         q = query(collectionRef, where(key.toLowerCase(), "==", value.toLowerCase()))
-    }else{
-         q = collectionRef  //no filter return all collection
-    }
-    const querySnapshot = await getDocs(q)
-    return querySnapshot.docs.map(doc => ({...doc.data(), id : doc.id}))
+type GetItemsFilters = {
+  available?: boolean
+  category?: string
+  tag?: string
 }
+
+export async function getItems(filters: GetItemsFilters = {}):Promise<DishProps[]> {
+  const constraints: QueryConstraint[] = []
+
+  if (filters.available !== undefined) {
+    constraints.push(where("available", "==", filters.available))
+  }
+
+  if (filters.category) {
+    constraints.push(where("category", "==", filters.category.toLowerCase()))
+  }
+
+  if (filters.tag) {
+    constraints.push(where("tags", "array-contains", filters.tag.toLowerCase()))
+  }
+
+  const q = constraints.length
+    ? query(collectionRef, ...constraints)
+    : collectionRef
+
+  const querySnapshot = await getDocs(q)
+  console.log("filters:", filters)
+  console.log("available type:", typeof filters.available)
+
+
+  return querySnapshot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data() as Omit<DishProps, "id">,
+  }))
+}
+
 
 //fecth one element form the collection that mathes the ID;client side
 
@@ -23,3 +50,20 @@ export async function getItemById(id:string){
     const jadeLilySnapshot = await getDoc(docRef)
     return {...jadeLilySnapshot.data(), id: jadeLilySnapshot.id}
 }
+
+
+
+
+
+/*export async function getItems(key?:string, value?:string){
+    let q
+    if(key && value){
+         q = query(collectionRef, where(key.toLowerCase(), "==", value))
+    }else{
+         q = collectionRef  //no filter return all collection
+    }
+    const querySnapshot = await getDocs(q)
+    return querySnapshot.docs.map(doc => ({...doc.data(), id : doc.id}))
+}*/
+
+
