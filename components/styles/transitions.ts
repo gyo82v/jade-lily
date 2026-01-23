262626c20fb8cae0889c198725c2998f3a2e3b52
gyo82v <gyo82v@gmail.com>
2026-01-23 19:39:49 +0100

    style: add new global styles for animations

--- components/styles/transitions.ts
+++ components/styles/transitions.ts
@@ -6,3 +6,21 @@ export const defaultTransition = `
    focus-visible:outline-none focus-visible:ring-3 active:bg-opacity-90
    disabled:opacity-50 disabled:pointer-events-none disabled:shadow-none disabled:scale-100
 `
+export const transitions = `
+  transition-transform transition-shadow transition-colors duration-300 ease-in-out
+  motion-reduce:transition-none motion-reduce:transform-none motion-reduce:duration-0
+`
+export const hoverEffects = `
+  hover:scale-105 hover:shadow-xl
+`
+export const activeEffects = `
+  active:scale-95 active:bg-opacity-90
+`
+export const focusEffects = `
+ focus-visible:ring-orange-200 focus-visible:ring-offset-2 
+ focus-visible:outline-none focus-visible:ring-3 
+`
+export const disabledEffects = `
+ disabled:opacity-50 disabled:pointer-events-none
+ disabled:shadow-none disabled:scale-100
+`
