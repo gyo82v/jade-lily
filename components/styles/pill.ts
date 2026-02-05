3dca51767c9c97b49c0caae90dd5b8e9a3fd883b
gyo82v <gyo82v@gmail.com>
2026-02-05 20:15:37 +0100

    style. update pill style

--- components/styles/pill.ts
+++ components/styles/pill.ts
@@ -6,3 +6,8 @@ export const pillStyle = `
         hover:shadow-md hover:text-orange-800
         active:scale-95 ${pillTransition} ${focusEffects}
     `
+export const pillLinkStyle = `
+        bg-white/80 text-orange-700 ring-1 ring-orange-200/60 shadow-sm cursor-pointer
+        hover:shadow-md hover:text-orange-800 active:scale-95 rounded-lg
+        w-6/12 flex items-center justify-center gap-2 py-2 
+        ${pillTransition} ${focusEffects}`
