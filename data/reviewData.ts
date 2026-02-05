c93c0539812de8249012ca767bf0c700f03fa32b
gyo82v <gyo82v@gmail.com>
2026-02-05 20:12:40 +0100

    feat: add data folder with reviews data

---
+++ data/reviewData.ts
@@ -0,0 +1,42 @@
+export const reviewData = [
+    {
+        rating : 5,
+        author : "Sarah Thompson",
+        date : "2025-03-15",
+        description : ` Jade Lily is a hidden gem! The avocado salad was fresh and flavorful,
+                        and the salmon steak was cooked to perfection. The ambiance is cozy
+                        and inviting. I’ll definitely be coming back soon!`
+    },
+    {
+        rating : 5,
+        author : "Mark Jenkins",
+        date : "2025-06-17",
+        description : ` This restaurant is simply amazing. The blueberry cheesecake was a dream,
+                        and the tiramisu was the best I’ve ever had. The staff is so friendly
+                        and attentive—it truly enhances the experience!`
+    },
+    {
+        rating : 5,
+        author : "Emily Rivera",
+        date : "2025-09-21",
+        description : ` An unforgettable dining experience! The pasta pesto had a perfect balance
+                        of flavors, and the beef steak melted in my mouth. Highly recommend
+                        for anyone who loves great food!`
+    },
+    {
+        rating : 5,
+        author : "James Carter",
+        date : "2025-10-15",
+        description : ` I’ve visited Jade Lily several times, and it never disappoints.
+                        The chicken wings are always juicy and delicious, and the cherry
+                        ice cream is an absolute must-try. They pay attention to every detail!`
+    },
+    {
+        rating : 5,
+        author : "Olivia Martinez",
+        date : "2025-11-15",
+        description : ` The perfect spot for foodies! The salad was fresh and vibrant,
+                        and the beef burger was packed with flavor. The decor is stylish
+                        yet comfortable—it’s my new favorite place to eat!`
+    }
+]
