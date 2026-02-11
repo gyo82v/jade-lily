ff972577abb6fea6007a2d57bfd5e0add0dd6094
gyo82v <gyo82v@gmail.com>
2026-02-11 18:56:19 +0100

    feat: update the account navbar

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -9,8 +9,16 @@ export default function AccountLayout({ children }: { children: React.ReactNode 
     return (
         <ClientProviders>
             <AuthGuard>
-                <div className="flex flex-col items-center mx-auto w-full">
+                <div className="flex flex-col items-center mx-auto w-full md:px-6 lg:px-10 xl:px-16">
                     <AccountNavbar />
+                    <div aria-hidden="true" className="w-full">
+                      <div className="hidden md:block max-w-5xl mx-auto px-6">
+                        <div className="h-px bg-gradient-to-r from-transparent via-orange-200/40 to-transparent" />
+                      </div>
+                      <div className="md:hidden">
+                       <div className="h-px bg-gradient-to-r from-transparent via-orange-200/40 to-transparent" />
+                      </div>
+                    </div>
                     <div className="flex flex-col flex-1 items-center w-full ">
                         {children}
                     </div>
