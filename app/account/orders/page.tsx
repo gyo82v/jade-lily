322124ebfe7786d2a4cc14bb7bf5f2926a61b94e
gyo82v <gyo82v@gmail.com>
2026-02-14 17:37:15 +0100

    feat: update the orders page

--- app/account/orders/page.tsx
+++ app/account/orders/page.tsx
@@ -2,8 +2,8 @@ import AccountOrdersPage from "../components/AccountOrdersPage"
 
 export default function OrdersPage() {
     return (
-        <section className="w-11/12 my-4">
-            <h1 className="text-xl font-bold">Yours orders:</h1>
+        <section className="w-full max-w-2xl mx-auto my-6 px-4">
+            <h1 className="text-xl font-bold mb-4">Yours orders:</h1>
             <AccountOrdersPage />
         </section>
     )
