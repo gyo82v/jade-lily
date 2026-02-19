8bb9105be7629100d70067a9852db5876bb04d55
gyo82v <gyo82v@gmail.com>
2026-02-19 19:51:26 +0100

    feat: update the menu layout

--- app/account/menu/layout/AccountMenuNavbar.tsx
+++ app/account/menu/layout/AccountMenuNavbar.tsx
@@ -4,12 +4,12 @@ export default function AccountMenuNavbar(){
     return(
         <nav>
             <ul className="flex gap-1">
+                <li><Link href="/account/menu">All</Link></li>
                 <li><Link href="/account/menu/mains"> mains</Link></li>
                 <li><Link href="/account/menu/burgers">burgers</Link></li>
                 <li><Link href="/account/menu/sides">sides</Link></li>
                 <li><Link href="/account/menu/desserts">desserts</Link></li>
                 <li><Link href="/account/menu/drinks">drinks</Link></li>
-                <li><Link href="/account/menu">reset</Link></li>
             </ul>
         </nav>
     )
