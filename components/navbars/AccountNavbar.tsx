2dc179bdbf681440d6040f3aa270a3695b0a14e4
gyo82v <gyo82v@gmail.com>
2026-02-20 20:13:24 +0100

    feat: update  account navbar

--- components/navbars/AccountNavbar.tsx
+++ components/navbars/AccountNavbar.tsx
@@ -35,7 +35,7 @@ export function AccountNavbar() {
       </div>
 
       {/* Mobile: scrollable compact strip */}
-      <div className="md:hidden ">
+      <div className="md:hidden border-2 border-orange-700 ">
         <div className="overflow-x-auto no-scrollbar">
           <div className="flex gap-7 px-4 py-3 mx-auto w-max">
             {items.map((it) => (
