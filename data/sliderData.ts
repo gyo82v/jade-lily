0157cde28d26eccb02b6c1c3623446dcf0637f45
gyo82v <gyo82v@gmail.com>
2026-02-26 18:33:38 +0100

    feat: add data for the slidder component

---
+++ data/sliderData.ts
@@ -0,0 +1,108 @@
+export const sliderData = [
+    {
+        available : true,
+        category : "mains",
+        description : `Succulent salmon steak topped with juicy shrimp,
+                       finished with lemon and fresh herbs. A delightful combination that’s
+                       sure to please seafood enthusiasts`,
+        imageUrlFull : "/dishes/full/mains/salmon-steack2-full.webp",
+        imageUrlThumb : "/dishes/thumb/mains/salmon-steack2-400.webp",
+        ingredients : "Salmon, shrimp, lemon, parsley, dill.",
+        name : "Shrimp Salmon",
+        origin : `Shrimp Salmon is a delightful fusion of flavors from the coastal
+                  regions of the Pacific Northwest. Juicy shrimp and tender salmon come
+                  together in this dish, enhanced by a light, flavorful sauce that
+                  highlights the natural sweetness of the seafood.`,
+        price : 11,
+        rating : 5,
+        type : "Regular"
+    },
+    {
+        available : true,
+        category : "sides",
+        description : `A reasonable portion of classic fries, perfectly seasoned
+                       and crisp. This timeless side dish is a must-have for any meal.`,
+        imageUrlFull : "/dishes/full/sides/fries-full.webp",
+        imageUrlThumb : "/dishes/thumb/sides/fries-400.webp",
+        ingredients : "Potatoes, salt, oil.",
+        name : "Simple Pleasure",
+        origin : `Simple Pleasure is a classic and comforting side of medium-sized fries,
+                  perfectly seasoned and crispy on the outside, soft on the inside. These
+                  fries are a timeless favorite that pairs well with any meal.`,
+        price : 4,
+        rating : 5,
+        type : "Starter"
+    },
+    {
+        available : true,
+        category : "mains",
+        description : `Traditional lasagna layered with a rich and savory bolognese sauce.
+                       This comforting dish is a favorite for its hearty flavors and cheesy goodness.`,
+        imageUrlFull : "/dishes/full/mains/lasagna-full.webp",
+        imageUrlThumb : "/dishes/thumb/mains/lasagna-400.webp",
+        ingredients : "Lasagna noodles, beef, tomatoes, cheese, herbs.",
+        name : "Bolognese Lasagna",
+        origin : `Bolognese Lasagna is a rich and comforting dish that comes from the
+                  heart of Bologna, Italy. Layers of pasta, savory bolognese sauce,
+                  creamy béchamel, and melted cheese create a decadent meal that has
+                  been a family favorite for generations.`,
+        price : 8,
+        rating : 5,
+        type : "Regular"
+    },
+    {
+        available : true,
+        category : "sides",
+        description : `A classic feta cheese salad with fresh lettuce, crunchy onion,
+                       juicy tomatoes, and olives. A delightful mix of flavors and textures
+                       inspired by Greek cuisine.`,
+        imageUrlFull : "/dishes/full/sides/feta-salad-full.webp",
+        imageUrlThumb : "/dishes/thumb/sides/feta-salad-400.webp",
+        ingredients : "Feta cheese, lettuce, onion, tomatoes, olives.",
+        name : "Feta Fusion",
+        origin : `Feta Fusion brings the flavors of the Mediterranean to your table.
+                  This salad features crumbled feta cheese, juicy cherry tomatoes,
+                  cucumbers, and red onions, all tossed in a lemon-oregano vinaigrette,
+                  offering a refreshing and tangy side dish.`,
+        price : 5,
+        rating : 5,
+        type : "Salad"
+    },
+    {
+        available : true,
+        category : "desserts",
+        description : `A classic premium tiramisu with layers of coffee-soaked ladyfingers,
+                       rich mascarpone cream, and a dusting of cocoa powder. An Italian dessert classic.`,
+        imageUrlFull : "/dishes/full/desserts/tiramisu-full.webp",
+        imageUrlThumb : "/dishes/thumb/desserts/tiramisu-400.webp",
+        ingredients : "Mascarpone, coffee, ladyfingers, cocoa.",
+        name : "Tiramisu Classic",
+        origin : `Tiramisu Classic is a timeless Italian dessert that originated
+                  in the region of Veneto. With its layers of coffee-soaked ladyfingers
+                  and creamy mascarpone, this beloved treat has a rich history of being
+                  the perfect end to any Italian meal, providing a harmonious blend of
+                  flavors and textures.`,
+        price : 6,
+        rating : 5,
+        type : "Choco"
+    },
+    {
+        available : true,
+        category : "desserts",
+        description : `A chocolate and cream gelato with three alternating layers
+                       of chocolate and cream, topped with a dusting of chocolate.
+                       A rich and indulgent treat.`,
+        imageUrlFull : "/dishes/full/desserts/choco-cream-cake-full.webp",
+        imageUrlThumb : "/dishes/thumb/desserts/choco-cream-cake-400.webp",
+        ingredients : "Chocolate, cream, cocoa.",
+        name : "Layered Joy",
+        origin : `Layered Joy is a multi-layered delight that was first served
+                  at an extravagant royal banquet in the 18th century. Each layer,
+                  from the delicate sponge cake to the luscious custard,
+                  is a testament to the artistry and elegance of traditional European desserts.`,
+        price : 5,
+        rating : 5,
+        type : "Choco"
+
+    }
+]
