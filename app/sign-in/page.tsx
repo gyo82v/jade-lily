1d0713c28011edce8731e9a22c5ec236fdc3dda0
gyo82v <gyo82v@gmail.com>
2026-02-28 18:34:36 +0100

    feat: add a layout to the page with a guest guard

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,16 +1,19 @@
 import { LoginForm } from "@/components/forms";
+import { focusEffects } from "@/components/styles";
 import ClientProviders from "@/firebase/ClientProviders";
 import Link from "next/link";
 
+
 export default function LoginPage(){
-    const link = `text-orange-700 font-bold ml-1`
+    const link = `text-orange-700 font-bold ml-1 ${focusEffects}`
 
     return(
         <ClientProviders>
-            <section className="p-4 mx-auto max-w-lg mt-14 flex flex-col w-full">
+            <section className={`p-4 grad-primary shadow-lg mx-auto h-102 md:mt-20
+                                 max-w-lg my-10 rounded-lg flex flex-col w-11/12`}>
                 <h1  className="text-xl font-bold">Sign in to your account</h1>
                 <LoginForm />
-                <p className="text-center">
+                <p className="text-center mt-4">
                     Dont have an account?
                     <Link href="/signup" className={link}>Create one now</Link>
                 </p>
