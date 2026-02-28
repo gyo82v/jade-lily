2dbd550b5ce7db4a681d14b88c5056227c7f0343
gyo82v <gyo82v@gmail.com>
2026-02-28 18:36:57 +0100

    feat: update the contact us page

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -2,7 +2,7 @@ import { ContactUsForm } from "@/components/forms"
 
 export default function ContactUsPage(){
     return(
-        <section className="flex flex-col w-11/12 mx-auto">
+        <section className="flex flex-col w-11/12 items-center mx-auto my-10">
             <h1 className="text-xl font-bold text-center">Contact us</h1>
             <ContactUsForm />
         </section>
